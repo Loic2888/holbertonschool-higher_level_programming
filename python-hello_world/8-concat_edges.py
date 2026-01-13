04e75e7e844c2dae57a572dad786b475d4bb6694
Loic2888 <12188@holbertonstudents.com>
2026-01-13 13:13:59 +0100

    fix: error, print str only

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
+str = f"{str}"[39:-54] + " " + f"{str}"[107:-18] + " " + f"{str}"[:-123]
 
-print(f"{str}"[39:-54] + " " + f"{str}"[107:-18] + " " + f"{str}"[:-123])
+print(str)
