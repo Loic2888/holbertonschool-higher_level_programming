925679c6112aec66d4bd8f8ed3d1b67eab6020b7
Loic2888 <12188@holbertonstudents.com>
2026-01-13 15:57:43 +0100

    fix: don't work, try another thing

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import random
 numb = random.randint(-10000, 10000)
-last_digit = numb % 10 if numb >= 0 else (numb % 10 - 10)
+last_digit = numb % 10 if numb >= 0 else (numb % -10)
 if last_digit > 5:
     print(f"Last digit of {numb} is {last_digit} and is greater than 5")
 elif last_digit == 0:
