dd275d812f7aec729bb690a8faabf3c34c7b23a9
Loic2888 <12188@holbertonstudents.com>
2026-01-13 13:17:35 +0100

    fix: error in the select caract in the string

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = f"{str}"[39:-54] + " " + f"{str}"[107:-18] + " " + f"{str}"[:-123]
+str = f"{str}"[39:-63] + " " + f"{str}"[107:-18] + " " + f"{str}"[:-123]
 print(str)
