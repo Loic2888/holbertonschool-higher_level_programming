00bf4dcedfd49a643a2f4ccdbbad2f7b24d38fd4
Loic2888 <12188@holbertonstudents.com>
2026-01-13 15:51:40 +0100

    fix: abs not working in the negate number, try another soluc

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 import random
-number = random.randint(-10000, 10000)
-last_digit = abs(number % 10)
+numb = random.randint(-10000, 10000)
+last_digit = numb % 10 if numb >= 0 else (numb % 10 - 10)
 if last_digit > 5:
-    print(f"Last digit of {number} is {last_digit} and is greater than 5")
+    print(f"Last digit of {numb} is {last_digit} and is greater than 5")
 elif last_digit == 0:
-    print(f"Last digit of {number} is {last_digit} and is 0")
+    print(f"Last digit of {numb} is {last_digit} and is 0")
 else:
-    print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
+    print(f"Last digit of {numb} is {last_digit} and is less than 6 and not 0")
