f6d2a2a955c06f841b8c1034cb61491eaa911071
Loic2888 <12188@holbertonstudents.com>
2026-01-13 10:59:51 +0100

    feat: script that concatenate two string

---
+++ python-hello_world/6-concat.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# This script concatenates two strings with a space in between and prints the result.
+str1 = "Holberton"
+str2 = "School"
+result = str1 + " " + str2
+print(f"Welcome to {result}!")
