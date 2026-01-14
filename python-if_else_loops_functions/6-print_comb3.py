3b9563d2160b69d1544cb49837b25b79ef559939
Loic2888 <12188@holbertonstudents.com>
2026-01-14 16:07:21 +0100

    fix: fucking .append

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-combinations = []
 for i in range(10):
     for j in range(i + 1, 10):
-        combinations.append(f"{i}{j}")
-print(", ".join("{:s}".format(combo) for combo in combinations))
+        print("{:d}{:d}".format(i, j), end="")
+        if not (i == 8 and j == 9):
+            print(", ", end="")
