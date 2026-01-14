6356b350fee4d09618b79adb913af72483440be2
Loic2888 <12188@holbertonstudents.com>
2026-01-14 16:04:30 +0100

    fix: fucking .format

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -3,4 +3,4 @@ combinations = []
 for i in range(10):
     for j in range(i + 1, 10):
         combinations.append(f"{i}{j}")
-print(", ".join(combinations))
+print(", ".join("{:s}".format(combo) for combo in combinations))
