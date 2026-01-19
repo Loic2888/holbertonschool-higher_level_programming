d80c0ddfd8641b3d09762c8585a7bb4c5db532b8
Loic2888 <12188@holbertonstudents.com>
2026-01-19 18:23:25 +0100

    fix: fail again, try some sorcelery

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,15 +1,5 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if not tuple_a and not tuple_b:
-        return (0, 0)
-    if len(tuple_a) or len(tuple_b) > 2:
-        tuple_a = tuple_a[:2]
-        tuple_b = tuple_b[:2]
-        while len(tuple_a) < 2:
-            tuple_a += (0,)
-        while len(tuple_b) < 2:
-            tuple_b += (0,)
-    else:
-        tuple_a = (0, 0)
-        tuple_b = (0, 0)
+    tuple_a = tuple_a + (0, 0)[:2 - len(tuple_a)]
+    tuple_b = tuple_b + (0, 0)[:2 - len(tuple_b)]
     return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
