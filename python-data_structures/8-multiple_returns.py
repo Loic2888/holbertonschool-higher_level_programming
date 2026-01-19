fe687e2733616a968cbae4e97383d5bd0d2b9707
Loic2888 <12188@holbertonstudents.com>
2026-01-19 18:34:59 +0100

    fix: fail again, try sorcellery

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length = len(sentence)
-    first = sentence[0]
-    if not sentence:
-        length = 0
-        first = None
+    first = None if length == 0 else sentence[0]
     return length, first
