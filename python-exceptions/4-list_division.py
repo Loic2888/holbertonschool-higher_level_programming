d40bb13d269a5edf5bdcce6f3f1b53c6926ae3a0
Loic2888 <12188@holbertonstudents.com>
2026-01-21 18:02:26 +0100

    fix: fail, i think it's pycodestyle

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -6,7 +6,8 @@ def list_division(my_list_1, my_list_2, list_length):
         try:
             a = my_list_1[i]
             b = my_list_2[i]
-            if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
+            if (not isinstance(a, (int, float)) or
+                    not isinstance(b, (int, float))):
                 print("wrong type")
             else:
                 result = a / b
@@ -15,6 +16,9 @@ def list_division(my_list_1, my_list_2, list_length):
             result = 0
         except IndexError:
             print("out of range")
+            result = 0
+        except TypeError:
+            print("wrong type")
             result = 0
         finally:
             new_list.append(result)
