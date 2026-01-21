3675029312ebbf8700052d311095f0ab0a8cc629
Loic2888 <12188@holbertonstudents.com>
2026-01-21 17:05:32 +0100

    feat: function that use try, and print the integer of a list and pass the str

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    printed = 0
+    count = 0
+    while count < x:
+        try:
+            print("{:d}".format(my_list[count]))
+            printed += 1
+        except (TypeError, ValueError):
+            pass
+        except IndexError:
+            break
+        count += 1
+    return printed
