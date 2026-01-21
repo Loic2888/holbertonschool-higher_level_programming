4d0550c74d27223b797fadf237f7746e75a1e92f
Loic2888 <12188@holbertonstudents.com>
2026-01-21 16:50:08 +0100

    fix: fail robot, don't know why, try again

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        nombre = int(value)
-        print("{:d}".format(nombre))
+        print("{:d}".format(value))
         return True
-    except ValueError:
+    except (ValueError, TypeError):
         return False
