1bc4ae6b1a46d02b393ea9e213a546d26d27bb2e
Loic2888 <12188@holbertonstudents.com>
2026-01-23 13:30:04 +0100

    feat: function that add 2 intefers and test.txt

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Docstring for 0-add_integer
+"""
+
+
+def add_integer(a, b=98):
+    """
+    Docstring for add_integer
+    :param a: First integer to sum
+    :param b: Second integer to sum
+    """
+    if a is None:
+        raise TypeError("a must be an integer")
+    if type(a) is not int and type(a) is not float:
+        raise TypeError("a must be an integer")
+    if type(b) is not int and type(b) is not float:
+        raise TypeError("b must be an integer")
+    a = int(a)
+    b = int(b)
+    return a + b
