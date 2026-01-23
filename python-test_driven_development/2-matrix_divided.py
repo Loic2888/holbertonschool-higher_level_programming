5766ee21f26b87de9741436e1984ee7c20ec8283
Loic2888 <12188@holbertonstudents.com>
2026-01-23 14:54:31 +0100

    fix: pycodestyle, clean it

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -3,27 +3,28 @@
 Docstring for 2-matrix_divided
 """
 
+
 def matrix_divided(matrix, div):
     """
     Divides all elements of a matrix by a given divisor.
-    
+
     Args:
         matrix (list): A list of lists of integers or floats.
         div (int/float): The number to divide by.
-        
+
     Returns:
         list: A new matrix with the result of the division.
-        
+
     Raises:
         TypeError: If matrix is not a list of lists of integers/floats.
         TypeError: If each row of the matrix is not of the same size.
         TypeError: If div is not a number.
         ZeroDivisionError: If div is 0.
     """
-    
+
     # 1. Validate Matrix Type (Must be list of lists of ints/floats)
-    type_error_msg = "matrix must be a matrix (list of lists) of integers/floats"
-    
+    type_error_msg = "matrix must be a matrix of integers/floats"
+
     if not isinstance(matrix, list) or len(matrix) == 0:
         raise TypeError(type_error_msg)
 
@@ -36,7 +37,7 @@ def matrix_divided(matrix, div):
 
     # 2. Validate Row Sizes (Must be consistent)
     size_error_msg = "Each row of the matrix must have the same size"
-    
+
     row_length = len(matrix[0])
     for row in matrix:
         if len(row) != row_length:
@@ -53,5 +54,5 @@ def matrix_divided(matrix, div):
     # 5. Perform Division and Return New Matrix
     # Using list comprehension to create a new matrix with rounded values
     new_matrix = [[round(x / div, 2) for x in row] for row in matrix]
-    
+
     return new_matrix
