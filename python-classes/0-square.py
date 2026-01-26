d32003468e7e7596af2d04b28aaa201ac581db81
Loic2888 <12188@holbertonstudents.com>
2026-01-26 16:00:07 +0100

    feat: add the task 1, 2, 3 and 4

---
+++ python-classes/0-square.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+"""Defines a Square class."""
+class Square:
+    pass
---
+++ python-classes/1-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""Defines a Square class, and init it"""
+class Square:
+
+    def __init__(self, size):
+        self.__size = size
