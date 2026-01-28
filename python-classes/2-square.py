f58c64c17f3acf70622deb9a6aeb668e47ba5dce
Loic2888 <12188@holbertonstudents.com>
2026-01-28 12:05:32 +0100

    fix: pycodestyle, add soc and blanc line

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
+"""Define a class Square based on 2-square.py."""
+
+
 class Square:
     """define a classe square, init it and raise error"""
     def __init__(self, size=0):
+        """Initialize a new Square."""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
