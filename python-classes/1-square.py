632e05a6af26c7d6f7cb766dc71b58b4b9e8ff0d
Loic2888 <12188@holbertonstudents.com>
2026-01-28 12:06:57 +0100

    fix: pycodestyle, add doc and blanc line

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """Defines a Square class, and init it"""
-class Square:
 
+
+class Square:
+    """Represent a square."""
     def __init__(self, size):
         self.__size = size
