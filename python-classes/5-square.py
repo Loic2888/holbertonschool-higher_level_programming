a0db1491c62fed83aad543fb5264f30219a023d8
Loic2888 <12188@holbertonstudents.com>
2026-01-28 12:14:40 +0100

    feat: add the function that print the square

---
+++ python-classes/5-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Define a class Square based on 3-square.py."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size=0):
+        """Initialize a new Square."""
+        self.size = size
+
+    @property
+    def size(self):
+        """Getter for the private size attribute."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter for the private size attribute."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return the current area of the square."""
+        return self.__size ** 2
+
+    def my_print(self):
+        """print the square with '#'"""
+        for _ in range size:
+            print("#" * self.__size)
+        if self.__size == 0:
+            print("")
