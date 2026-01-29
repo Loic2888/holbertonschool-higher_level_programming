9cae439b03a6440df2dd01535ae611383b63dcde
Loic2888 <12188@holbertonstudents.com>
2026-01-29 13:07:54 +0100

    feat: creat a class empty

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""define a rectangle class"""
+
+
+class Rectangle:
+    """
+    Classe Rectangle.
+
+    Représente un rectangle défini par sa largeur et sa hauteur.
+    """
+    pass
