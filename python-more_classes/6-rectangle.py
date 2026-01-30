871b1e4a4049d93876cda9114680d832f553a672
Loic2888 <12188@holbertonstudents.com>
2026-01-30 15:25:29 +0100

    fix: add the class variable

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -10,6 +10,7 @@ class Rectangle:
     Uses private properties with validation to ensure width and height
     are non-negative integers.
     """
+    number_of_instances = 0
 
     def __init__(self, width=0, height=0):
         """
