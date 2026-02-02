9b3f9990ef2b94a0463873d4ff96863f1cd6ec09
Loic2888 <12188@holbertonstudents.com>
2026-02-02 15:43:31 +0100

    feat: follo task 6, add methode that raise value

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module that defines a base geometry class"""
+
+class BaseGeometry:
+    """BaseGeometry class"""
+    def area(self):
+        """Method that raises an exception"""
+        raise Exception("area() is not implemented")
+    def integer_validator(self, name, value):
+        """Method that validates value"""
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
