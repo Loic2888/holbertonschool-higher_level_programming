c2f557b833dd79c371426ad9a14ab02c3080b14c
Loic2888 <12188@holbertonstudents.com>
2026-02-04 16:18:12 +0100

    fix: fail isinstance don't work, try whit issubclass

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""Module that defines the inherits_from function."""
+
 
 def is_kind_of_class(obj, a_class):
-    return isinstance(obj, a_class)
+    """Function that returns True if the object is an instance of a class
+    that is or inherited (directly or indirectly) from the specified class;"""
+    return issubclass(type(obj), a_class)
