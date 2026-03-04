9039ed302c08e1fd6cf06188e5c93d9653dc15f2
Loic2888 <12188@holbertonstudents.com>
2026-03-04 15:30:05 +0100

    feat: script that create a tables in the current DB

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,5 @@
+-- Create a table named `first_table` with two columns: `id` of type INT and `name` of type VARCHAR(256).
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+)
