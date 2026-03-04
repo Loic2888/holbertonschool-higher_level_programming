231b78e8a44010c634e3bf7d96acdfb72bc9106b
Loic2888 <12188@holbertonstudents.com>
2026-03-04 19:49:09 +0100

    feat: query that computes the average score of all record

---
+++ SQL_introduction/14-average.sql
@@ -0,0 +1,3 @@
+-- Computes the average score of all records in the table second_table
+-- Database name will be passed as an argument of the mysql command
+SELECT AVG(score) AS average FROM second_table;
