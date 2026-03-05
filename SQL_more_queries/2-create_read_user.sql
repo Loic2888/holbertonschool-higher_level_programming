493ab8d931580f6e25565c6b03ee1c08a9dc90de
Loic2888 <12188@holbertonstudents.com>
2026-03-05 13:26:07 +0100

    fix: try adding ident in the creat

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -7,7 +7,8 @@ IF NOT EXISTS `hbtn_0d_2`;
 -- Create the user user_0d_2 with password user_0d_2_pwd
 
 CREATE USER
-IF NOT EXISTS 'user_0d_2'@'localhost';
+IF NOT EXISTS 'user_0d_2'@'localhost'
+IDENTIFIED BY 'user_0d_2_pwd';
 
 -- The user_0d_2 has SELECT privilege on hbtn_0d_2 with password user_0d_2_pwd
 
