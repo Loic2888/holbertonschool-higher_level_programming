7fb74195ffa156734ab476a770d42b21394f6c97
Loic2888 <12188@holbertonstudents.com>
2026-03-05 13:13:10 +0100

    fix: add doc in the two query

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,6 @@
 -- 2-create_read_user.sql
 -- Creates the database hbtn_0d_2 and the user user_0d_2
--- The user_0d_2 has SELECT privilege on hbtn_0d_2 with password user_0d_2_pwd
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+-- Create The user_0d_2 has SELECT privilege on hbtn_0d_2 with password user_0d_2_pwd
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'hbtn_0d_2_pwd';
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
