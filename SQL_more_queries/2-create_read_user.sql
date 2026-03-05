6fc9f3b191177ad1f76998fd5d050016f7323646
Loic2888 <12188@holbertonstudents.com>
2026-03-05 13:21:32 +0100

    fix: try adding wite space

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,7 +1,20 @@
 -- 2-create_read_user.sql
--- Creates the database hbtn_0d_2 and the user user_0d_2
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
--- Create The user_0d_2 has SELECT privilege on hbtn_0d_2 with password user_0d_2_pwd
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'hbtn_0d_2_pwd';
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';;
+-- Creates the database hbtn_0d_2 
+
+CREATE DATABASE
+IF NOT EXISTS `hbtn_0d_2`;
+
+-- Create the user user_0d_2 with password user_0d_2_pwd
+
+CREATE USER
+IF NOT EXISTS 'user_0d_2'@'localhost'
+IDENTIFIED BY 'user_0d_2_pwd';
+
+-- The user_0d_2 has SELECT privilege on hbtn_0d_2 with password user_0d_2_pwd
+
+GRANT SELECT
+ON `hbtn_0d_2`.*
+TO 'user_0d_2'@'localhost'
+IDENTIFIED BY 'user_0d_2_pwd';
+
 FLUSH PRIVILEGES;
