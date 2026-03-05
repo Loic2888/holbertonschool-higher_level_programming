10fb839eb20145ea8c1c6316a48f6abc4fcfcb7d
Loic2888 <12188@holbertonstudents.com>
2026-03-05 12:58:12 +0100

    fix: fail checker, try without flush

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,3 @@
 -- Script to create MySQL user user_0d_1 with all privileges (idempotent)
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
