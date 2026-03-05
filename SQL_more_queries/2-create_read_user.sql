c1b241dab638cc97e17db17c7e69df97ab663f9e
Loic2888 <12188@holbertonstudents.com>
2026-03-05 13:14:56 +0100

    fix: try adding the flush privileges

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -4,3 +4,4 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- Create The user_0d_2 has SELECT privilege on hbtn_0d_2 with password user_0d_2_pwd
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'hbtn_0d_2_pwd';
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
