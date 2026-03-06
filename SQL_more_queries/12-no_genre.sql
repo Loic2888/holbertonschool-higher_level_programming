bd67e6c817b2cf21a771d1f9a10f2b1a98157451
Loic2888 <12188@holbertonstudents.com>
2026-03-06 15:36:47 +0100

    feat: query that Import the database dump from hbtn_0d_tvshows to your MySQL server: download

---
+++ SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,10 @@
+-- Lists all shows in hbtn_0d_tvshows without a genre linked
+-- Format: tv_shows.title - tv_show_genres.genre_id (NULL)
+-- Sorted by tv_shows.title and tv_show_genres.genre_id
+
+SELECT s.title, g.genre_id
+FROM tv_shows AS s
+LEFT JOIN tv_show_genres AS g
+ON s.id = g.show_id
+WHERE g.genre_id IS NULL
+ORDER BY s.title, g.genre_id;
