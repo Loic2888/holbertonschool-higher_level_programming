f528f34b707b9447bd1b9925e0acaa45188407da
Loic2888 <12188@holbertonstudents.com>
2026-03-06 15:39:31 +0100

    feat: query that Import the database dump from hbtn_0d_tvshows to your MySQL server: download

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,12 @@
+-- Lists all genres and number of shows linked
+-- Columns: genre - number_of_shows
+-- No genres without shows
+-- Sorted by number_of_shows DESC
+
+SELECT g.name AS genre,
+       COUNT(*) AS number_of_shows
+FROM tv_genres AS g
+INNER JOIN tv_show_genres AS t
+ON g.id = t.genre_id
+GROUP BY g.name
+ORDER BY number_of_shows DESC;
