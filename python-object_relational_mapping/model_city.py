643cec75f1c780dbb3ce03e657e5ded385ec5a4e
Loic2888 <12188@holbertonstudents.com>
2026-03-06 18:52:03 +0100

    feat: add the task 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14

---
+++ python-object_relational_mapping/model_city.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Model City class"""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base
+
+
+class City(Base):
+    """City class that links to table states"""
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
